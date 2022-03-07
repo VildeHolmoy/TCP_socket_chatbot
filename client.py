6450dfa2acf769e1a18dfab1a118093777438cc9
VildeHolmoy <s187161@oslomet.no>
2022-03-07 16:46:57 +0100

    First commit. Rules and current plan for implementation.

---
+++ client.py
@@ -0,0 +1,9 @@
+import socket
+
+
+# Need to make connection to server
+
+# Need a loop as long as the client is connected do this ...
+
+# Need input parameter, with possible exit code
+
---
+++ server.py
@@ -0,0 +1,18 @@
+import socket
+
+
+# Need to setup connection to client server
+
+# Need to make 5 bots
+# The bots should have one of them to take input from client,
+# then the rest should respond in a dialog.
+
+# Rules:
+# The server should accept any connection e.g there can be more than one user on client??
+# It should be a sort of "chat room"
+# All responses should be sent back to all clients (except the one who sent it? (from assignment))
+# There should be a list of connected clients
+# I can decide when and how to disconnect clients
+# one bot should take response from command line
+# The bots are not the first to speak, but they will always respond
+#
